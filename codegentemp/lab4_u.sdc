57ca2ea7a16375ee42d90a75783c48c2937e1e76
ippersky <80413598+ippersky@users.noreply.github.com>
2021-03-10 16:13:56 -0500

    Partie 1
    Partie 1

--- codegentemp/lab4_u.sdc
+++ codegentemp/lab4_u.sdc
@@ -1,3 +1,3 @@
 # Component constraints for C:\Users\marie\Documents\Polytechnique\Session 4 (hiver 2021)\GBM2100 Projets\Labo\Laboratoire4\lab4\lab4.cydsn\TopDesign\TopDesign.cysch
 # Project: C:\Users\marie\Documents\Polytechnique\Session 4 (hiver 2021)\GBM2100 Projets\Labo\Laboratoire4\lab4\lab4.cydsn\lab4.cyprj
-# Date: Wed, 10 Mar 2021 19:38:44 GMT
+# Date: Wed, 10 Mar 2021 20:55:58 GMT
--- main_cm4.c
+++ main_cm4.c
@@ -16,12 +16,31 @@
 #include "params.h"
 #include "queue.h"
 
+void vLedTask(void *arg){
+    for(;;){
+        Cy_GPIO_Write(LED_0_PORT, LED_0_NUM, 1);
+        vTaskDelay(pdMS_TO_TICKS(500));
+        Cy_GPIO_Write(LED_0_PORT, LED_0_NUM, 0);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+
+
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
+    
+    // Partie 1
+    xTaskCreate(vLedTask, "Led Task", 400, NULL, 1, NULL);
+    vTaskStartScheduler(); 
+    
+    // Partie 2
+    
+    
+    
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
     for(;;)
     {
         /* Place your application code here. */
