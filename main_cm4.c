9cad649abe22283ebe4b8e3f44501df27f7a383f
ippersky <80413598+ippersky@users.noreply.github.com>
2021-03-10 16:38:41 -0500

    Partie 2 - 5
    Jusqu'au point 5

--- main_cm4.c
+++ main_cm4.c
@@ -16,6 +16,8 @@
 #include "params.h"
 #include "queue.h"
 
+volatile SemaphoreHandle_t bouton_semph;
+
 void vLedTask(void *arg){
     for(;;){
         Cy_GPIO_Write(LED_0_PORT, LED_0_NUM, 1);
@@ -25,17 +27,37 @@ void vLedTask(void *arg){
     }
 }
 
+void isr_bouton(void){
+    xSemaphoreGiveFromISR(bouton_semph, NULL);
+    Cy_GPIO_ClearInterrupt(Bouton_0_PORT, Bouton_0_NUM);
+    NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
+}
+
+void bouton_task(){
+    xSemaphoreTake(bouton_semph);
+    vTaskDelay(pdMS_TO_TICKS(20));
+    
+    
+    
+    
+}
 
 
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
+    Cy_SysInt_Init(&Bouton_ISR_cfg, isr_bouton);
+    NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
+    NVIC_EnableIRQ(Bouton_ISR_cfg.intrSrc);
+        
     
     // Partie 1
     xTaskCreate(vLedTask, "Led Task", 400, NULL, 1, NULL);
     vTaskStartScheduler(); 
     
     // Partie 2
+    
+    
     
     
     
